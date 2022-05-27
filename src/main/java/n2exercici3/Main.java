90c3175e690cdaf07bcf8ee43cae7efd4d4d3272
ATomas87 <kombayn_nikoladze@msn.com>
2022-05-27 13:42:14 +0200

    Terminados todos los niveles

--- src/main/java/n2exercici3/Main.java
+++ src/main/java/n2exercici3/Main.java
@@ -2,21 +2,13 @@ package n2exercici3;
 
 public class Main {
     public static void main(String[] args) {
-        Operator operatorSum = (n1, n2) -> {
-            return n1 + n2;
-        };
+        Operator operatorSum = (n1, n2) -> n1 + n2;
 
-        Operator operatorSub = (n1, n2) -> {
-            return n1 - n2;
-        };
+        Operator operatorSub = (n1, n2) -> n1 - n2;
 
-        Operator operatorMul = (n1, n2) -> {
-            return n1 * n2;
-        };
+        Operator operatorMul = (n1, n2) -> n1 * n2;
 
-        Operator operatorDiv = (n1, n2) -> {
-            return n1 / n2;
-        };
+        Operator operatorDiv = (n1, n2) -> n1 / n2;
 
         System.out.println(operatorSum.operation(2,5));
         System.out.println(operatorSub.operation(2,5));
